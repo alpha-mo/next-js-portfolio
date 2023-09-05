3ab2ab955c9036259389d5d3a89806c38a8b1c38
Mohamad Ojail <mohamad.ojail@gmail.com>
2023-09-05 15:34:50 +0200

    404 add

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main>
+      <p className="w-fit p-6 border border-divider-200 text-content-100 rounded-lg m-auto">The page you are requesting does not exist</p>
+    </main>
+  );
+}
