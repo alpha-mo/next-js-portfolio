cbb2e41594e42ae7faa0bcfbab7b045e52d0ee57
Mohamad Ojail <mohamad.ojail@gmail.com>
2023-09-06 06:55:26 +0200

    cv update

--- src/components/homePage/ui/CvBtn.tsx
+++ src/components/homePage/ui/CvBtn.tsx
@@ -4,16 +4,31 @@ import Link from "next/link";
 
 const CvBtn = async () => {
   return (
-    <Link
-      download={"ojailcv /files/OjailCV.pdf"}
-      href={"files/OjailCV.pdf"}
-      title="Ojail-CV"
-      target="_blank"
-      prefetch={false}
-      className="text-content-100 border border-divider-200 rounded-md w-full hover:shadow-nav hover:bg-base-hover py-1 flex justify-center items-center"
-    >
-      Download my CV
-    </Link>
+    <div className="text-content-100 w-full py-1 flex justify-center items-center gap-4 border border-divider-200 rounded-lg">
+      <p>Download my CV</p>
+      <div className="flex gap-2">
+        <Link
+          download={"Ojail-CV /files/cv/en/en-Ojail-CV.pdf"}
+          href={"/files/cv/en/en-Ojail-CV.pdf"}
+          title="English version"
+          target="_blank"
+          prefetch={false}
+          className="border border-divider-200 hover:bg-base-hover px-2 rounded-md hover:shadow-nav"
+        >
+          En
+        </Link>
+        <Link
+          download={"Ojail-CV /files/cv/sv/sv-Ojail-CV.pdf"}
+          href={"/files/cv/sv/sv-Ojail-CV.pdf"}
+          title="Svenska"
+          target="_blank"
+          prefetch={false}
+          className="border border-divider-200 hover:bg-base-hover px-2 rounded-md hover:shadow-nav"
+        >
+          Sv
+        </Link>
+      </div>
+    </div>
   );
 };
 
